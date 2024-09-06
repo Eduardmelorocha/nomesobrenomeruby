14c51291fbe4f23f335c20c45b6b27f1e4af2387
Eduardo Melo <edumelobr7@gmail.com>
2024-09-05 21:39:29 -0300

    commit

---
+++ nome.rb
@@ -0,0 +1,10 @@
+puts "Seja bem vindo! Por favor preencher os dados abaixo."
+
+puts "Digite seu nome:  "
+nome = gets.chomp
+puts "digite seu sobrenome: "
+sobrenome = gets.chomp
+puts "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá #{nome} #{sobrenome}! Seja bem vindo sua idade é #{idade}"
